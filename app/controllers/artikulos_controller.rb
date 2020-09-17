1468c65e43adaf4ea8d72f1c0243b1da38a0e78a
Benjamin Malabanan <benm@roxc.net>
2020-09-17 10:45:42 +0800

    added the edit and update pages

--- app/controllers/artikulos_controller.rb
+++ app/controllers/artikulos_controller.rb
@@ -7,4 +7,41 @@ class ArtikulosController < ApplicationController
     @artikulos = Artikulo.all
   end
 
+  def new
+    @artikulo = Artikulo.new
+  end
+
+  def create
+    #render plain: params[:artikulo]  #check the params being pass
+
+    @artikulo = Artikulo.new(params.require(:artikulo).permit(:title, :description))
+    #render plain: @artikulo.inspect  #check the object
+
+    if @artikulo.save  
+      flash[:notice]  = "Galing, nakagawa na Artikulo!" 
+
+      #redirect_to artikulo_path(@artikulo)  #ver1
+      redirect_to @artikulo    #ver 2
+    else
+      render 'new'      
+    end
+
+  end
+
+  def edit
+    @artikulo = Artikulo.find(params[:id])
+  end
+  
+  def update
+    #byebug
+    @artikulo = Artikulo.find(params[:id])
+    if @artikulo.update(params.require(:artikulo).permit(:title, :description))
+      flash[:notice]  = "Ok, na save ko na ung bagong Artikulo!" 
+      redirect_to @artikulo
+    else
+      render 'edit'
+    end
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :articles
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :artikulos, only: [:show, :index]
+  resources :artikulos, only: [:show, :index, :new , :create, :edit, :update]
 end
+
